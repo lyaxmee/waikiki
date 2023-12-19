c5000092e7bdc1c76a12c9027d1802ca3746aeaf
Laxmi Timilsina <laxmitimilsina65@gmail.com>
2023-12-19 21:48:37 +0545

    change

---
+++ app.js
@@ -0,0 +1,34 @@
+const searchicon1 = document.querySelector('#searchicon1');
+const srchicon1 = document.querySelector('#srchicon1');
+const search1 = document.querySelector('#searchinput1');
+
+searchicon1.addEventListener('click',function(){
+    search1.style.display = 'flex';
+    searchicon1.style.display = 'none';
+})
+
+const searchicon2 = document.querySelector('#searchicon2');
+const srchicon2 = document.querySelector('#srchicon2');
+const search2 = document.querySelector('#searchinput2');
+
+searchicon2.addEventListener('click',function(){
+    search2.style.display = 'flex';
+    searchicon2.style.display = 'none';
+})
+
+const bar = document.querySelector('.fa-bars');
+const cross = document.querySelector('#hdcross');
+const headerbar = document.querySelector('.headerbar');
+
+bar.addEventListener('click',function(){
+    setTimeout(() =>{
+        cross.style.display = 'block';
+    },200);
+    headerbar.style.right = '0%';
+})
+
+cross.addEventListener('click',function(){
+    cross.style.display = 'none';
+    headerbar.style.right = '-100%'
+})
+
